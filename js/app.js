9b74a3e247e2a6b73e526ae3bf2bb18b5f1521ac
nchlodzi <nchlodzi@gmail.com>
2016-03-31 02:49:59 +0200

    calculator almost finish

--- js/app.js
+++ js/app.js
@@ -66,13 +66,146 @@ $('#box2').on('mouseleave', function(){
 	})
 
 //dropdowns
-
-	$('.arrow-down').first().on('click', function(){
-		$('.drop').css('display', 'block');
-	})
 	
-	$('#check2').on('click', function(){
-		$('#check2').toggleClass('check3');
+	function calculator(){
+
+	$('.dropdowns .arrow-down').on('click', function(){
+		$(this).next('.drop').toggle();
+	});
+
+
+
+	var clair = $('.dropdowns').first().find('.drop li').first();
+		clair.on('click', function(){
+			$(this).parent().siblings('.dropLabel').html('Clair');
+			$('.title').html('Clair');
+			var namePrice = clair.data('price');
+			$('.title_chair').html(namePrice);
+			// $('.sum').html(namePrice);
+		sumSummary();
+		})
+
+
+	var margarita = $('.dropdowns').first().find('.drop li').eq(1);
+		margarita.on('click', function(){
+			$(this).parent().siblings('.dropLabel').html('Margarita');
+			$('.title').html('Margarita');			
+			var namePrice = margarita.data('price');
+			$('.title_chair').html(namePrice);
+			// $('.sum').html(namePrice);
+			sumSummary();
+		})
+
+	var selena = $('.dropdowns').first().find('.drop li').eq(2);
+	
+		selena.on('click', function(){
+			$(this).parent().siblings('.dropLabel').html('Selena');
+			$('.title').html('Selena');
+			var namePrice = selena.data('price');			
+			$('.title_chair').html(namePrice);
+			// $('.sum').html(namePrice);
+			sumSummary();
+		})
+
+
+	var czerwony = $('.dropdowns').eq(1).find('.drop li').first();
+	
+		czerwony.on('click', function(){
+			$(this).parent().siblings('.dropLabel').html('Czerwony');
+			$('.color').html('Czerwony');
+			var colorPrice = czerwony.data('price');
+			$('.color_chair').html(colorPrice);
+			sumSummary();
+		})
+
+	var czarny = $('.dropdowns').eq(1).find('.drop li').eq(1);
+	
+		czarny.on('click', function(){
+			$(this).parent().siblings('.dropLabel').html('Czarny');
+			$('.color').html('Czarny');
+			var colorPrice = czarny.data('price');
+			$('.color_chair').html(colorPrice);
+
+			sumSummary();
+		})
+
+	var pomaranczowy = $('.dropdowns').eq(1).find('.drop li').eq(2);
+	
+		pomaranczowy.on('click', function(){
+			$(this).parent().siblings('.dropLabel').html('Pomarańczowy');
+			$('.color').html('Pomarańczowy');
+			var colorPrice = pomaranczowy.data('price');
+			$('.color_chair').html(colorPrice);
+
+			sumSummary();
+		})
+
+	var tkanina = $('.dropdowns').eq(2).find('.drop li').first();
+	
+		tkanina.on('click', function(){
+			$(this).parent().siblings('.dropLabel').html('Tkanina');
+			$('.pattern').html('Tkanina');
+			$('.pattern_chair').html(patternPrice);
+			var patternPrice = tkanina.data('price');
+			sumSummary();
+		})
+
+	var skora = $('.dropdowns').eq(2).find('.drop li').eq(1);
+
+	
+		skora.on('click', function(){
+			$(this).parent().siblings('.dropLabel').html('Skóra');
+			$('.pattern').html('Skóra');
+			$('.pattern_chair').html(patternPrice);
+			var patternPrice = skora.data('price');
+			sumSummary();
+		})
+
+
+	var check2 = $('#check2');
+
+	var transportPrice = check2.data('price');
+	check2.on('click', function(){
+		check2.toggleClass('check3');
+
+		if (check2.hasClass('check3')){
+			$('.transport').html('Transport');		
+			$('.transport_chair').html(transportPrice);
+
+		} else{
+			$('.transport').html('Transport');
+			$('.transport_chair').html("0");
+		}
+		sumSummary();
 	})
 
+
+	function sumSummary(){
+		// var sum = 0;
+
+		// $('.sum').html(
+		// 	parseInt(namePrice1) + 
+		// 	parseInt(colorPrice1) + 
+		// 	parseInt(colorPrice2) + 
+		// 	parseInt(colorPrice3) + 
+		// 	parseInt(patternPrice) +  
+		// 	parseInt(transportPrice)
+		// 	) +" zł";
+		// return sum;
+	
+}	
+	// var sum = $('.sum');
+
+	var namePrice = $('.title_chair').html();
+	var colorPrice = $('.color_chair').html();
+	var patternPrice = $('.pattern_chair').html();
+	var transportPrice = $('.transport_chair').html();
+
+	$('.sum').html(parseInt(namePrice) 
+				 + parseInt(colorPrice) 
+				 + parseInt(patternPrice) 
+				 + parseInt(transportPrice));
+
+	}
+	calculator();
 });
